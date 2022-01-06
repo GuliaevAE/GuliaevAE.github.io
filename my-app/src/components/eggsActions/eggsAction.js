e846dc6d565d3fa233c66ba07b1077eb84a478e2
Гуляев Антон <63814006+slavynin911@users.noreply.github.com>
2022-01-06 21:48:13 +0300

    Мелкие правки

--- my-app/src/components/eggsActions/eggsAction.js
+++ my-app/src/components/eggsActions/eggsAction.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import EggsNone from "../eggs";
 import "../eggsActions/eggsActions.css";
 
+
 export default class EggsActions extends Component {
     constructor(props) {
         super(props);
